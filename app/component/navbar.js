5a04b2b4df98ee69f5d5642a353212a5e60461f9
indic-mfaizk <mohd.faiz@indicchain.com>
2023-09-11 15:10:07 +0530

    image bug solved

--- app/component/navbar.js
+++ app/component/navbar.js
@@ -56,154 +56,160 @@ function NavBar() {
   };
 
   return (
-    <Container maxWidth="xl">
-      <AppBar
-        position="static"
-        sx={{
-          background: "linear-gradient(90deg, #160a13,#220d29,#1d0c20)",
-          pb: "10px",
-        }}
-      >
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
+    <AppBar
+      position="static"
+      sx={{
+        background: "linear-gradient(90deg, #160a13,#220d29,#1d0c20)",
+        // bgcolor: "blue",
+        pb: "10px",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <Image
+              src={logo}
+              alt=""
+              style={styles.logo}
+              onClick={() => {
+                navClickHandler("home");
               }}
-            >
-              <Image
-                src={logo}
-                alt=""
-                style={styles.logo}
-                onClick={() => {
-                  navClickHandler("home");
-                }}
-              />
-            </Typography>
+            />
+          </Typography>
 
-            <Box
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+              //   bgcolor: "blue",
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
               sx={{
-                flexGrow: 1,
-                display: { xs: "flex", md: "none" },
-                //   bgcolor: "blue",
+                display: { xs: "block", md: "none" },
+                zIndex: "100",
               }}
             >
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                  zIndex: "100",
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem
-                    key={page}
-                    onClick={(e) => {
-                      handleCloseNavMenu(e);
-                      navClickHandler(page);
-                    }}
-                  >
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              <img src={logo} alt="" style={styles.logo} />
-            </Typography>
-            <Box
-              sx={{
-                flexGrow: 1,
-                display: { xs: "none", md: "flex" },
-                alignItems: "center",
-                justifyContent: "flex-end",
-              }}
-            >
-              {pages.map((page) =>
-                page === "Eco System" ? (
-                  <></>
-                ) : (
-                  <Button
-                    key={page}
-                    onClick={(e) => {
-                      handleCloseNavMenu(e);
-                      navClickHandler(page);
-                    }}
-                    sx={{ my: 2, color: "white", display: "block" }}
-                  >
-                    {page}
-                  </Button>
-                )
-              )}
-              <Button sx={{ bgcolor: "#a6482f", textAlign: "center", ml: 3 }}>
-                <img src={eco} alt="" style={styles.ecoButton} />
-                <Typography
-                  variant="button"
-                  color={"white"}
-                  px={"10px"}
-                  fontFamily={"sora"}
-                  fontWeight={"400"}
+              {pages.map((page) => (
+                <MenuItem
+                  key={page}
                   onClick={(e) => {
                     handleCloseNavMenu(e);
-                    navClickHandler("eco system");
+                    navClickHandler(page);
                   }}
                 >
-                  Eco System
-                </Typography>
-              </Button>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </Container>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <Image
+              src={logo}
+              alt=""
+              style={styles.logo}
+              onClick={() => {
+                navClickHandler("home");
+              }}
+            />
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              alignItems: "center",
+              justifyContent: "flex-end",
+            }}
+          >
+            {pages.map((page) =>
+              page === "Eco System" ? (
+                <></>
+              ) : (
+                <Button
+                  key={page}
+                  onClick={(e) => {
+                    handleCloseNavMenu(e);
+                    navClickHandler(page);
+                  }}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              )
+            )}
+            <Button sx={{ bgcolor: "#a6482f", textAlign: "center", ml: 3 }}>
+              <Image src={eco} alt="" style={styles.ecoButton} />
+              <Typography
+                variant="button"
+                color={"white"}
+                px={"10px"}
+                fontFamily={"sora"}
+                fontWeight={"400"}
+                onClick={(e) => {
+                  handleCloseNavMenu(e);
+                  navClickHandler("eco system");
+                }}
+              >
+                Eco System
+              </Typography>
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 export default NavBar;
